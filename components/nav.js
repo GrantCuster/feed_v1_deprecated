bd0cef38db4309205609a5f2d106cb9cae560452
Grant Custer <grantcuster@gmail.com>
2017-06-15 23:13:27 -0400

    nav adjustment

--- components/nav.js
+++ components/nav.js
@@ -4,34 +4,34 @@ import Link from "next/link";
 export default ({ url }) => {
 	return (
 		<div className="flex justify-between mb2">
-			<div className="px2 bold">
+			<div className="p1 bold">
 				<Link prefetch href="/">
-					<a className="no-underline py2 block">Grant Custer</a>
+					<a className="no-underline p1 block">Grant Custer</a>
 				</Link>
 			</div>
-			<div className="flex px1">
-				<div className="px1">
+			<div className="flex p1 flex-wrap">
+				<div className="">
 					<Link prefetch href="/">
-						<a className={`${url.pathname === "/" ? "link-active" : ""} py2 block`}>Feed</a>
+						<a className={`${url.pathname === "/" ? "link-active" : ""} p1 block`}>Feed</a>
 					</Link>
 				</div>
-				<div className="px1">
+				<div className="">
 					<Link prefetch href="/info">
-						<a className={`${url.pathname === "/info" ? "link-active" : ""} py2 block`}>Info</a>
+						<a className={`${url.pathname === "/info" ? "link-active" : ""} p1 block`}>Info</a>
 					</Link>
 				</div>
-				<div className="px1">
+				<div className="">
 					<Link prefetch href="/writing">
-						<a className={`${url.pathname === "/writing" ? "link-active" : ""} py2 block`}>Writing</a>
+						<a className={`${url.pathname === "/writing" ? "link-active" : ""} p1 block`}>Writing</a>
 					</Link>
 				</div>
-				<div className="px1">
+				<div className="">
 					<Link prefetch href="/misc">
-						<a className={`${url.pathname === "/misc" ? "link-active" : ""} py2 block`}>Misc</a>
+						<a className={`${url.pathname === "/misc" ? "link-active" : ""} p1 block`}>Misc</a>
 					</Link>
 				</div>
-				<div className="px1">
-					<a className="py2 block" href="http://twitter.com/grantcuster">Twitter→</a>
+				<div className="">
+					<a className="p1 block" href="http://twitter.com/grantcuster">Twitter→</a>
 				</div>
 			</div>
 		</div>
