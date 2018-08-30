bd7d43cccbb75a4580290a3cfc1df2e5854985c8
Grant Custer <grantcuster@gmail.com>
2018-08-29 22:29:37 -0400

    timeline finishing up

--- pages/timeline.js
+++ pages/timeline.js
@@ -15,6 +15,8 @@ import {
 } from 'date-fns'
 import { slugDate } from '../utils/utils-general.js'
 
+let chunk_size = 50
+
 export default class extends React.Component {
   static async getInitialProps({ req, query }) {
     const baseUrl = makeBaseUrl(req)
@@ -24,15 +26,53 @@ export default class extends React.Component {
     return { feed_posts: display_posts }
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      chunks: 1,
+      width: null,
+    }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth })
+  }
+
+  loadMore() {
+    this.setState({ chunks: this.state.chunks + 1 })
+  }
+
+  loadAll() {
+    let max_chunks = Math.ceil(this.props.feed_posts.length / chunk_size)
+    this.setState({ chunks: max_chunks })
+  }
+
   render() {
     const { url, feed_posts } = this.props
-    let space = 40
-    let display_posts = feed_posts.slice(0, 50).reverse()
+    let chunk_limit = Math.min(
+      this.state.chunks * chunk_size,
+      feed_posts.length
+    )
+    let display_posts = feed_posts.slice(0, chunk_limit).reverse()
     let today = new Date()
     let today_month = startOfMonth(today)
     let end_date = subMonths(startOfMonth(display_posts[0].posted), 1)
     let months = differenceInMonths(today_month, end_date)
     let month_list = []
+
+    let last_post = display_posts[0]
+    let last_post_date = new Date(last_post.posted)
+    let last_days_offset = differenceInCalendarDays(today, last_post_date)
+
     for (let i = 0; i < months; i++) {
       if (i === 0) {
         let object = {
@@ -50,6 +90,16 @@ export default class extends React.Component {
         month_list.push(object)
       }
     }
+
+    let post_width = 33
+    let max_height = 22
+    let space = 40
+    if (this.state.width < 600) {
+      post_width = 50
+      max_height = 33
+      space = 20
+    }
+
     return (
       <div>
         <Head>
@@ -62,70 +112,119 @@ export default class extends React.Component {
         </div>
 
         <div className="measure-max image-max mx-auto px2 mb2">
-          My work posts spaced by date and time of day.
+          My work posts spaced by date and time of day.{' '}
+          {display_posts.length < feed_posts.length ? (
+            <button className="button-link" onClick={this.loadAll.bind(this)}>
+              Load all {feed_posts.length} images.
+            </button>
+          ) : null}
         </div>
-        <div
-          style={{
-            position: 'relative',
-          }}
-        >
-          <div>
-            {display_posts.map((post, i) => {
-              const date_slug = slugDate(post.posted)
-              let dated = new Date(post.posted)
-              let hours = parseInt(format(startOfHour(dated), 'H'))
-              let column = hours + 1
-              let days_offset = differenceInCalendarDays(today, dated)
-              return (
-                <Link
-                  href={`/feed_post_page?date_slug=${date_slug}`}
-                  as={`/post/${date_slug}`}
+        {this.state.width !== null ? (
+          <div
+            style={{
+              position: 'relative',
+            }}
+          >
+            <div
+              style={{
+                position: 'relative',
+                paddingTop: `${last_days_offset * space}px`,
+                width: '100%',
+              }}
+            >
+              <div style={{ width: `${post_width}%` }}>
+                <img
+                  src={last_post.img}
+                  style={{
+                    display: 'block',
+                    maxWidth: '100%',
+                    maxHeight: `${max_height}vw`,
+                    opacity: 0,
+                  }}
+                />
+              </div>
+              {display_posts.length < feed_posts.length ? (
+                <button
+                  className="button-link"
+                  style={{
+                    marginTop: space,
+                    padding: '1em 0',
+                    width: '100%',
+                    background: '#efefef',
+                  }}
+                  onClick={this.loadMore.bind(this)}
                 >
-                  <a
+                  Load more
+                </button>
+              ) : (
+                <div style={{ height: 57 }} />
+              )}
+            </div>
+            <div>
+              {display_posts.map((post, i) => {
+                const date_slug = slugDate(post.posted)
+                let dated = new Date(post.posted)
+                let hours = parseInt(format(startOfHour(dated), 'H'))
+                let column = hours
+                let days_offset = differenceInCalendarDays(today, dated)
+                return (
+                  <div
                     style={{
                       position: 'absolute',
                       display: 'block',
-                      left: `${(column / 24) * (100 - 33)}%`,
-                      width: '33%',
-                      top: `${days_offset * space}`,
+                      left: `${(column / 23) * (100 - post_width)}%`,
+                      width: `${post_width}%`,
+                      top: `${days_offset * space}px`,
                     }}
-                    title={dated.toLocaleString()}
                   >
-                    <img
-                      key={post.posted}
-                      src={post.img}
-                      style={{
-                        display: 'block',
-                        maxHeight: '22vw',
-                        maxWidth: '100%',
-                      }}
-                    />
-                  </a>
-                </Link>
-              )
-            })}
+                    <Link
+                      href={`/feed_post_page?date_slug=${date_slug}`}
+                      as={`/post/${date_slug}`}
+                    >
+                      <a
+                        style={{
+                          display: 'block',
+                        }}
+                        title={dated.toLocaleString()}
+                      >
+                        <img
+                          key={post.posted}
+                          src={post.img}
+                          style={{
+                            display: 'block',
+                            maxWidth: '100%',
+                            maxHeight: `${max_height}vw`,
+                          }}
+                        />
+                      </a>
+                    </Link>
+                  </div>
+                )
+              })}
+            </div>
+            <div>
+              {month_list.map((month, i) => {
+                return (
+                  <div
+                    className="px2"
+                    key={month.name}
+                    style={{
+                      position: 'absolute',
+                      display: 'block',
+                      left: `0`,
+                      width: `${post_width}%`,
+                      top: `${month.offset * space}px`,
+                      textTransform: 'uppercase',
+                      color: '#aaa',
+                    }}
+                  >
+                    {month.name}
+                  </div>
+                )
+              })}
+            </div>
           </div>
-          <div>
-            {month_list.map((month, i) => {
-              return (
-                <div
-                  className="px2"
-                  style={{
-                    position: 'absolute',
-                    display: 'block',
-                    left: `0`,
-                    width: '33%',
-                    top: `${month.offset * space}`,
-                    textTransform: 'uppercase',
-                    color: '#aaa',
-                  }}
-                >
-                  {month.name}
-                </div>
-              )
-            })}
-          </div>
-        </div>
+        ) : null}
       </div>
     )
   }
