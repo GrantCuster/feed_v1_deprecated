cd21adf258126393d0aa25d513e55b82c6ce9a60
Grant Custer <grantcuster@gmail.com>
2018-09-25 18:06:27 -0400

    more reason issues

--- pages/movies.js
+++ pages/movies.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Movies from '../components/Movies.bs'
+// import Movies from '../components/Movies.bs'
 
 class App extends Component {
   render() {
     return (
       <div className="measure-max mx-auto px2 py2">
-        <Movies message="grant" />
+        {/* <Movies message="grant" /> */}
       </div>
     )
   }
